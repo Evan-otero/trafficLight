6bf7e296034746b57da7e87308a4624a05ec5e42
Evan Otero <Evan.otero19@gmail.com>
2019-09-16 18:36:54 +0000

    created traffic light in html and css and altered setstates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,19 +5,40 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			clicked: null
+		};
+	}
+
 	render() {
+		let alpha = "";
+		if (this.state.clicked == "red") alpha = "selected";
+		let bravo = "";
+		if (this.state.clicked == "yellow") bravo = "selected";
+		let charlie = "";
+		if (this.state.clicked == "green") charlie = "selected";
 		return (
-			<div className="text-center mt-5">
-				<div id="wire">
-                </div>
-                <div id="box">
-                    <div className="light">
-                    </div>
-                    <div className="light">
-                    </div>
-                    <div className="light">
-                    </div>
-                </div>
+			<div className="text-center mt-5" id="wrapper">
+				<div id="wire" />
+				<div id="box">
+					<div
+						className="light"
+						id={"red" + alpha}
+						onClick={() => this.setState({ clicked: "red" })}
+					/>
+					<div
+						className="light"
+						id={"yellow" + bravo}
+						onClick={() => this.setState({ clicked: "yellow" })}
+					/>
+					<div
+						className="light"
+						id={"green" + charlie}
+						onClick={() => this.setState({ clicked: "green" })}
+					/>
+				</div>
 			</div>
 		);
 	}
